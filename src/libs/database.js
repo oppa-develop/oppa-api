e79fbc275422fbdeabb156f32f0492c3d4d50f74
javier muñoz <x_jemd_x@hotmail.com>
2021-02-09 22:09:10 -0300

    cambio momentaneo para ver conf de la bdd

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
-
-const pool = mysql.createPool({
+const dbConfig = {
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.DB_PORT
-});
+}
+
+console.log({dbConfig});
+
+const pool = mysql.createPool(dbConfig);
 
 pool.getConnection((err, connection) => {
   if (err) {

52fc6e51366e1d1bdb70d0f774293934dc017924
javier muñoz <x_jemd_x@hotmail.com>
2021-02-10 22:52:13 -0300

    cambio en la query para comprobar si el usuario existe al loguearse para traer id de roles

--- src/models/auth.models.js
+++ src/models/auth.models.js
@@ -3,7 +3,7 @@ const pool = require('../libs/database');
 let authModel = {};
 
 authModel.getUserByEmail = async (email) => {
-  const [user] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
+  const [user] = await pool.query('SELECT admin_id, client_id, provider_id, users.* FROM users LEFT JOIN admins ON admins.users_user_id = user_id LEFT JOIN clients ON clients.users_user_id = user_id LEFT JOIN providers ON providers.users_user_id = user_id WHERE email = ?', [email]);
   return user
 }
 

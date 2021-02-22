53b10b526a500427317decce7ff294c66472c5af
javier muñoz <x_jemd_x@hotmail.com>
2021-02-22 16:20:03 -0300

    login de clientes responde usuario con elders y todos sus datos

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -7,14 +7,20 @@ authModel.getClientByEmail = async (email) => {
   try {
     conn = await pool.getConnection();
     await conn.beginTransaction();
-    let [user] = await conn.query('SELECT admin_id, client_id, provider_id, users.* FROM users LEFT JOIN admins ON admins.users_user_id = user_id LEFT JOIN clients ON clients.users_user_id = user_id LEFT JOIN providers ON providers.users_user_id = user_id WHERE email = ?', [email]);
+    let [user] = await conn.query(`SELECT admin_id, client_id, provider_id, users.*, (SELECT total FROM wallet_movements INNER JOIN users ON users.user_id = users_user_id WHERE users.email = ? ORDER BY wallet_movements_id DESC LIMIT 1) AS 'credit' FROM users LEFT JOIN admins ON admins.users_user_id = user_id LEFT JOIN clients ON clients.users_user_id = user_id LEFT JOIN providers ON providers.users_user_id = user_id WHERE users.email = ?;`, [email, email]);
     if (user[0].client_id) {
       const [addresses] = await conn.query('SELECT * FROM addresses WHERE users_user_id = (SELECT user_id FROM users LEFT JOIN admins ON admins.users_user_id = user_id LEFT JOIN clients ON clients.users_user_id = user_id LEFT JOIN providers ON providers.users_user_id = user_id WHERE email = ?);', [email]);
-      const [credit] = await conn.query('SELECT total FROM wallet_movements WHERE wallet_movements.users_user_id = ? ORDER BY created_at DESC LIMIT 1', [user[0].user_id]);
-      const [elders] = await conn.query('SELECT admin_id, client_id, provider_id, users.* FROM users LEFT JOIN admins ON admins.users_user_id = user_id LEFT JOIN clients ON clients.users_user_id = user_id LEFT JOIN providers ON providers.users_user_id = user_id WHERE client_id in (SELECT clients_has_clients.senior_client_id FROM clients_has_clients WHERE user_client_id = ?);', [user[0].client_id])
+      const [elders] = await conn.query(`SELECT admin_id, client_id, provider_id, users.user_id AS 'uid', users.*, (SELECT total FROM wallet_movements INNER JOIN users ON users.user_id = users_user_id WHERE wallet_movements.users_user_id = uid ORDER BY wallet_movements_id DESC LIMIT 1) AS 'credit' FROM users LEFT JOIN admins ON admins.users_user_id = user_id LEFT JOIN clients ON clients.users_user_id = user_id LEFT JOIN providers ON providers.users_user_id = user_id WHERE client_id in (SELECT clients_has_clients.senior_client_id FROM clients_has_clients WHERE user_client_id = ?);`, [user[0].client_id]);
       user[0].addresses = addresses;
       user[0].elders = elders;
-      user[0].credit = credit[0]?.total || 0;
+      let i = 0;
+      user[0].elders.forEach(async elder => {
+        const [elderAddresses] = await conn.query('SELECT * FROM addresses WHERE users_user_id = ?;', [elder.user_id]);
+        user[0].elders[i].addresses = elderAddresses;
+        i++;
+      });
+
+      user[0].credit = user[0].credit;
     } else {
       throw Error('User is not a client')
     }

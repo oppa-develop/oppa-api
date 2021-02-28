b87bb11f609f4c3ba1908236da8b88e0410e012a
javier muñoz <x_jemd_x@hotmail.com>
2021-02-28 19:45:08 -0300

    conectando a bdd con key privada

--- src/libs/database.js
+++ src/libs/database.js
@@ -4,7 +4,10 @@ const pool = mysql.createPool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: process.env.SSL_CA
+  }
 });
 
 pool.getConnection((err, connection) => {

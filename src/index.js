be5d2f0eae046b2945721405d5b28406067059fc
javier muñoz <x_jemd_x@hotmail.com>
2021-06-08 18:32:51 -0400

    cambiando swagger al nuevo dominio

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const swaggerOptions = {
         description: 'Development server (local with test data).'
       },
       {
-        url: process.env.HOST + '/api',
+        url: 'https://api.somosoppa.cl/api',
         description: 'Development server (online with test data).'
       }
     ]

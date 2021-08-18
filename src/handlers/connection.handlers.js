addd555bb4b07e980c5d0ca5e6daf7db0d3b1fb6
Javier Muñoz <x_jemd_x@hotmail.com>
2021-08-18 17:33:10 -0400

    avances en nueva implementación de websockets

--- src/handlers/connection.handlers.js
+++ src/handlers/connection.handlers.js
@@ -2,7 +2,7 @@ module.exports = (io, socket) => {
   console.log('WS: User connected:', socket.handshake.query.firstname, socket.handshake.query.lastname);
 
   // agregamos al usuario a una sala de notificaciones
-  socket.join('user_id_' + socket.user_id);
+  socket.join('user_id_' + socket.handshake.query.user_id);
 
   // acciones al desconectar
   socket.on('disconnect', () => {
--- src/handlers/notification.handlers.js
+++ src/handlers/notification.handlers.js
@@ -2,6 +2,6 @@ module.exports = (io, socket) => {
   // cuando una notificación es recibida, se envía al usuario
   socket.on('notification', data => {
     console.log('WS: notification: ', data);
-    if (data.destination) socket.to('user_id_' + data.destination).broadcast.emit('notification', data);  
+    if (data.destination) socket.to('user_id_' + data.destination).broadcast.emit('notification', data);
   });
 }

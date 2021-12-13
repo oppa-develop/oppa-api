328b95d22e0906f5f17f86f77a335ff60e3a7fda
javier muñoz <x_jemd_x@hotmail.com>
2021-12-13 19:26:42 -0300

    fix obtener servicios al solicitar agendar servicio

--- src/models/services.model.js
+++ src/models/services.model.js
@@ -44,17 +44,15 @@ servicesModel.getPotentialServices = async (potentialProviders, service_id, regi
   let potentialServices = []
   
   return new Promise(async resolve => {
-    let i = 0
+    
     for await (let provider of potentialProviders) {
       // borramos la data sensible
       delete provider.password;
       delete provider.token;
-
-      console.log(pool.format(`SELECT * FROM provider_has_services WHERE provider_has_services.providers_provider_id = ? AND provider_has_services.services_service_id = ? AND provider_has_services.state = 'active' AND (provider_has_services.gender = 'Unisex' OR provider_has_services.gender = ?);`, [provider.provider_id, service_id, gender]))
   
       const [provider_has_services] = await pool.query(`SELECT * FROM provider_has_services WHERE provider_has_services.providers_provider_id = ? AND provider_has_services.services_service_id = ? AND provider_has_services.state = 'active' AND (provider_has_services.gender = 'Unisex' OR provider_has_services.gender = ?);`, [provider.provider_id, service_id, gender]);
 
-      provider_has_services.filter(async provider_has_service => {
+      for await (let provider_has_service of provider_has_services) {
         // obtenemos la locación definida por el proveedor para este servicio, filtrada por region, comuna y género
         const [location] = await pool.query(`SELECT * FROM locations WHERE locations.provider_has_services_provider_has_services_id = ? AND locations.region = ? AND (locations.district = ? OR locations.district IS NULL);`, [provider_has_service.provider_has_services_id, region, district]);
         provider_has_service.location = location;
@@ -74,16 +72,14 @@ servicesModel.getPotentialServices = async (potentialProviders, service_id, regi
   
         // finalmente, comprobamos que todos los filtros sean true
         if (filters.date && filters.hour) potentialServices.push(provider_has_service)
-        if (i === potentialProviders.length - 1) resolve(potentialServices)
-        i++
-
+  
         // devolvemos los filtros a estado false
         filters.date = false;
         filters.hour = false;
-      })
-
+      }
     }
-    console.log({potentialProviders})
+
+    resolve(potentialServices)
   })
 }
 

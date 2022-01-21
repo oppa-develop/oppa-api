c70b57e33f7cae8e0f37c1303cada981adc40832
oppa-develop <96750501+oppa-develop@users.noreply.github.com>
2022-01-21 11:23:41 -0300

    agregando variables para apuntar tbk a producción

--- src/routes/transbank.routes.js
+++ src/routes/transbank.routes.js
@@ -1,82 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const TransaccionCompleta = require('transbank-sdk').TransaccionCompleta;
-const dayjs = require('dayjs');
-const cert = require('../cert/normal');
-const WebPay = require('webpay-nodejs');
 const WebpayPlus = require('transbank-sdk').WebpayPlus;
-
-const wp = new WebPay({
-  commerceCode: cert.commerceCode,
-  publicKey: cert.publicKey,
-  privateKey: cert.privateKey,
-  webpayKey: cert.webpayKey,
-  verbose: true,
-  env: WebPay.ENV.INTEGRACION
-})
-
-let transaction;
-let transactions = {};
-let transactionsByToken = {};
-let url = process.env.HOST + '/api/transbank';
-
-router.post('/create', async (req, res) => {
-  let buyOrder = "CLTBK" + dayjs().format('YYYYMMDDHHmmss');
-  let sessionId = "S-" + dayjs().format('YYYYMMDDHHmmss');
-
-  const {
-    cvv,
-    cardNumber,
-    month,
-    year,
-    amount,
-    type
-  } = req.body
-
-  console.log(req.body)
-
-  try {
-    const createResponse = (type === 'credit') ?
-      await TransaccionCompleta.Transaction.create(buyOrder, sessionId, amount, cvv, cardNumber, year + "/" + month) :
-      await TransaccionCompleta.Transaction.create(buyOrder, sessionId, amount, undefined, cardNumber, year + "/" + month);
-
-    let transactionData = {
-      buyOrder,
-      sessionId,
-      amount,
-      createResponse,
-    };
-
-    res.status(200).json({
-      success: true,
-      message: 'Se creó la transacción con el objetivo de obtener un identificador único.',
-      transactionData
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      message: 'Error en la transacción.',
-      error
-    });
-  }
+const path = require('path');
+require('dotenv').config({
+  path: path.join(__dirname, '../.env')
 });
 
-router.post('/confirm', async (req, res) => {
-  const {
-    transactionToken,
-  } = req.body
-
-  const confirmResponse = await TransaccionCompleta.Transaction.commit(transactionToken);
-
-  res.status(200).json({
-    success: true,
-    message: 'Transacción confirmada correctamente.',
-    confirmResponse
-  });
-});
-
-// endpoints de WebpayPlus
+WebpayPlus.commerceCode = process.env.TBK_COMMERCE_CODE;
+WebpayPlus.apiKey = process.env.TBK_KEY;
+WebpayPlus.environment = process.env.TBK_ENVIRONMENT;
 
 /**
  * @swagger
